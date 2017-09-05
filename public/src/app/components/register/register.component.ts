f0c96b2dd604d7f7b66e3f9cdd7e50ed21841ffa
Ghanshyam Baviskar <gnbaviskar@gmail.com>
2017-09-05 18:59:23 +0530

    update

--- public/src/app/components/register/register.component.ts
+++ public/src/app/components/register/register.component.ts
@@ -22,30 +22,64 @@ export class RegisterComponent implements OnInit {
       username: ['',Validators.compose([
         Validators.required,
         Validators.minLength(5),
-        Validators.maxLength(30)
+        Validators.maxLength(30),
+        this.validateUser
       ])],
-      email: ['',Validators.required],
+      email: ['',Validators.compose([
+        Validators.required,
+        this.validateEmail
+      ])],
       password: ['',Validators.compose([
         Validators.required,
         Validators.minLength(5),
-        Validators.maxLength(35)
+        Validators.maxLength(35),
+        this.validatePassword
       ])],
       confirmPassword: ['',Validators.required]
-    }
-    // {
-    //   validator: this.matchingPassword('password','confirm')
-    // });
+    },
+    {
+      validator: this.matchingPassword('password','confirmPassword')
+    });
   }
 
-  // matchingPassword(password, confirm){
-  //   return (group: FormGroup) => {
-  //     if(group.controls[password].value === group.controls[confirm].value){
-  //       return null;
-  //     }else{
-  //       return {'matchingPassword': true}
-  //     }
-  //   }
-  // }
+  validateEmail(controls){
+    const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if(regExp.test(controls.value)){
+      return null;
+    }else{ 
+      return { 'validateEmail': true }
+    }
+  }
+
+  validateUser(controls){
+    const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+    if(regExp.test(controls.value)){
+      return null;
+    }else{
+      return { 'validateUser': true }
+    }
+  }
+
+  validatePassword(controls){
+    const regExp = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,35})/);
+    if(regExp.test(controls.value)){
+      return null;
+    }else{
+      return { 'validatePassword': true }
+    }
+  }
+
+  matchingPassword(password, confirmPassword){
+    return (group: FormGroup) => {
+      if(group.controls[password].value === group.controls[confirmPassword].value){
+        return null;
+      }else{
+        return {'matchingPassword': true}
+      }
+    }
+  }
+
+
 
 
   onRegisterSubmit(){
